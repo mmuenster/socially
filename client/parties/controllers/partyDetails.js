948a6848fe4ba1ebc9881e6a102c8f5bd94f7b08
Matthew Muenster <matt.muenster@gmail.com>
2015-04-04 17:36:51 -0400

    first commit

---
+++ client/parties/controllers/partyDetails.js
@@ -0,0 +1,21 @@
+
+angular.module("socially").controller("PartyDetailsCtrl", ['$scope', '$stateParams', '$meteor',
+  function($scope, $stateParams, $meteor){
+
+    //$scope.party = $meteor.object(Parties, $stateParams.partyId, false);
+    $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
+    $scope.party = $meteor.object(Parties, $stateParams.partyId).subscribe('parties');
+
+    $scope.save = function() {
+      $scope.party.save().then(function(numberOfDocs){
+        console.log('save success doc affected ', numberOfDocs);
+      }, function(error){
+        console.log('save error', error);
+      });
+    };
+
+    $scope.reset = function() {
+      $scope.party.reset();
+    };
+
+}]);
---
+++ server/parties.js
@@ -0,0 +1,13 @@
+Meteor.publish("parties", function (options) {
+  return Parties.find({
+    $or:[
+      {$and:[
+        {"public": true},
+        {"public": {$exists: true}}
+      ]},
+      {$and:[
+        {owner: this.userId},
+        {owner: {$exists: true}}
+      ]}
+    ]}, options);
+});
